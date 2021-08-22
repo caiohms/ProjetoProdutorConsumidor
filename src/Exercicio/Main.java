62e702cd215b1654e847de2b9b32eb1e4045f949
caiohms <44868897+caiohms@users.noreply.github.com>
2021-08-21 23:18:51 -0300

    versao 3

--- src/Exercicio/Main.java
+++ src/Exercicio/Main.java
@@ -2,7 +2,7 @@ package Exercicio;
 
 import java.util.concurrent.Semaphore;
 
-// Versão 2
+// Versão 3
 
 public class Main {
 
@@ -28,15 +28,43 @@ public class Main {
         Semaphore espacosFilaEntregas = new Semaphore(tamanhoFila);
 
         Loja lojaA = new Loja("A", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaB = new Loja("B", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaC = new Loja("C", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaD = new Loja("D", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaE = new Loja("E", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaF = new Loja("F", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaG = new Loja("G", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
+        Loja lojaH = new Loja("H", filaVendas, mutexVendas, itensFilaVendas, espacosFilaVendas);
 
         Fabricante fabricanteA = new Fabricante("A", filaVendas, filaEntregas, mutexVendas, itensFilaVendas,
+                espacosFilaVendas, mutexEntregas, itensFilaEntregas, espacosFilaEntregas);
+        Fabricante fabricanteB = new Fabricante("B", filaVendas, filaEntregas, mutexVendas, itensFilaVendas,
+                espacosFilaVendas, mutexEntregas, itensFilaEntregas, espacosFilaEntregas);
+        Fabricante fabricanteC = new Fabricante("C", filaVendas, filaEntregas, mutexVendas, itensFilaVendas,
+                espacosFilaVendas, mutexEntregas, itensFilaEntregas, espacosFilaEntregas);
+        Fabricante fabricanteD = new Fabricante("D", filaVendas, filaEntregas, mutexVendas, itensFilaVendas,
                 espacosFilaVendas, mutexEntregas, itensFilaEntregas, espacosFilaEntregas);
 
         Transportadora transportadoraA = new Transportadora("A", mutexEntregas, itensFilaEntregas,
                 espacosFilaEntregas, filaEntregas);
+        Transportadora transportadoraB = new Transportadora("B", mutexEntregas, itensFilaEntregas,
+                espacosFilaEntregas, filaEntregas);
 
         lojaA.start();
+        lojaB.start();
+        lojaC.start();
+        lojaD.start();
+        lojaE.start();
+        lojaF.start();
+        lojaG.start();
+        lojaH.start();
+
         fabricanteA.start();
+        fabricanteB.start();
+        fabricanteC.start();
+        fabricanteD.start();
+
         transportadoraA.start();
+        transportadoraB.start();
     }
 }
